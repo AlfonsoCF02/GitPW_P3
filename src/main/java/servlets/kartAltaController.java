073a1211182284ea0221fba2d33a2b4b3e73d85e
Soyalfonsoenpw <alfonsodlt9@gmail.com>
2022-12-08 01:06:58 +0100

    altakart

--- src/main/java/servlets/kartAltaController.java
+++ src/main/java/servlets/kartAltaController.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import business.kart.kartstat;
+import business.pista.GestorPistas;
+
 /**
  * Servlet implementation class kartAltaController
  */
@@ -34,7 +37,27 @@ public class kartAltaController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String tipo=request.getParameter("tipo");
+	    String estado=request.getParameter("estado");
+       kartstat ks;
+	   if(tipo!=null && !tipo.equals("")){
+		   if(estado.equals(kartstat.disponible.toString())){
+			   ks=kartstat.disponible;
+		       else if(estado.equals(kartstat.mantenimiento.toString())) {
+					   ks=kartstat.mantenimiento;
 
+		       }else{
+				   ks=kartstat.reservado;
+		       }
+	        }
+		   GestorPistas gk=new GestorPistas();
+		   gk.crearKart(null, tipo, ks);
+		   if(sw){
+		   	request.getRequestDispatcher("Mensaje.jsp").forward(request, response);
+		   }else{
+	    	PrintWriter out=response.getWriter();
+	    	out.println("Si estas viendo este mensaje es por que algo salio mal, no se pudo completar tu solicitud.");
+	       }
+	   }
+	}
 }
---
+++ src/main/webapp/mvc/view/kartAltaView.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<jsp:useBean  id="customerBean" scope="session" class="display.javabean.CustomerBean"></jsp:useBean>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Log in</title>
+</head>
+<body>
+<%
+/* Posibles flujos:
+	1) customerBean está logado (!= null && != "") -> Se redirige al index.jsp (no debería estar aquí pero hay que comprobarlo)
+	2) customerBean no está logado:
+		a) Hay parámetros en el request  -> procede del controlador /con mensaje 
+		b) No hay parámetros en el request -> procede del controlador /sin mensaje
+	*/
+String nextPage = "../../index.jsp";
+String messageNextPage = request.getParameter("message");
+%>
+<form id="formkart" method="post" action="kartAlta">
+	<label for="tipo">Tipo: </label>
+	<input type="text" name="tipo" placeholder="Enter type false/true" required><br/>
+	<label for="estado">Estado: </label>
+	<input type="text" name="estado" placeholder="disponible/reservado/mantenimiento" required><br/>	
+	<br/>
+	<input type="submit" value="Submit">
+</form>
+</body>
+</html>
