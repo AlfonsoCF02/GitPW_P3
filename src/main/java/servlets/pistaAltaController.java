2892049e46aeab44ddb6c51a4aae8994e6f3e195
Soyalfonsoenpw <alfonsodlt9@gmail.com>
2022-12-09 21:52:19 +0100

    Update pistaAltaController.java

--- src/main/java/servlets/pistaAltaController.java
+++ src/main/java/servlets/pistaAltaController.java
@@ -28,16 +28,17 @@ public class pistaAltaController extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
+    
+    /**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
-	/**
+    /**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -87,5 +88,4 @@ public class pistaAltaController extends HttpServlet {
 			}
 		}
 	}
-
 }
